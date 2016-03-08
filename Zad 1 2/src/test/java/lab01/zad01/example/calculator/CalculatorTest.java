fd78ae7cbfd457dec73ef2b3baf6ea4f398df306
Wysocki Patryk <wysockipawelpatryk@gmail.com>
2016-03-08 11:16:23 +0100

    pierwsze dwa zadania

---
+++ Zad 1 2/src/test/java/lab01/zad01/example/calculator/CalculatorTest.java
@@ -0,0 +1,38 @@
+package lab01.zad01.example.calculator;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	//class under test
+	private Calculator calc = new Calculator();
+	
+	@Test
+	public void addingTest() {
+		assertNull((calc.add(1, 1) == 2 ? null : 1));
+	}
+	@Test
+	public void substractingTest() {
+		assertEquals(5, calc.sub(7, 2));
+	}
+	@Test
+	public void multiplyingTest() {
+		assertEquals(6, calc.multi(3, 2));
+	}
+	@Test(expected = ArithmeticException.class)
+	public void div0Test() {
+		calc.div(6, 0);
+	}
+	@Test
+	public void divTest() {
+		assertThat(calc.div(6, 2), is(equalTo(0)));
+	}
+	@Test
+	public void greaterTest() {
+		assertThat(calc.greater(3, 6), is(equalTo(false)));
+	}
+
+}
---
+++ Zad 1 2/src/test/java/lab01/zad02/example/calculator/CalculatorTest.java
@@ -0,0 +1,34 @@
+package lab01.zad02.example.calculator;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	//class under test
+	private Calculator calc = new Calculator();
+	
+	@Test
+	public void addingTest() {
+		assertNull((calc.add(1, 1) == 2 ? null : 1));
+	}
+	@Test
+	public void substractingTest() {
+		assertEquals(5, calc.sub(7, 2), 0.001);
+	}
+	@Test
+	public void multiplyingTest() {
+		assertEquals(6, calc.multi(3, 2), 0.001);
+	}
+	@Test
+	public void divTest() {
+		assertEquals(3, calc.div(6, 2), 0.001);
+	}
+	@Test
+	public void greaterTest() {
+		assertThat(calc.greater(3, 6), is(equalTo(false)));
+	}
+
+}
